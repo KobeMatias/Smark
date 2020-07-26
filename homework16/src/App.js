57c8808d54837a0ccfff98cfa31e80b6eba9af40
Kobe Matias <kobematias@yahoo.com>
2020-07-26 16:35:04 -0400

    working

---
+++ homework16/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
